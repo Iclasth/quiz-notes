86adb47b25d17debd9a91ce2067227639485b106
iclasTH <iclasinprogramacao@gmail.com>
2026-04-27 12:21:24 -0300

    fix(main): Refatorando as widgets criadas diretamente no main dart
    O objetivo de refatorar as classes padrões criadas no "main.dart" foi para facilitar na escrita e manutenção de toda home page.

--- quiz_notes/test/widget_test.dart
+++ quiz_notes/test/widget_test.dart
@@ -9,11 +9,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:quiz_notes/main.dart';
+import 'package:quiz_notes/views/quiz_notes.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(QuizNotes());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
